33fcec20e368edef005cee3048ef6f87c071007f
unknown <iq000012@gmail.com>
2025-11-08 16:32:22 +0500

    DummyLinear device param

--- CustomLayers/DummyLinear.py
+++ CustomLayers/DummyLinear.py
@@ -1,10 +1,11 @@
 import torch
 import torch.nn as nn
 
+
 class DummyLinear(nn.Module):
-    def __init__(self, l):
+    def __init__(self, l, device):
         super(DummyLinear, self).__init__()
-        self.weight = nn.Parameter(l.weight.clone())
-    
+        self.weight = nn.Parameter(l.weight.clone()).to(device)
+
     def forward(self, x):
         return torch.matmul(x, self.weight.T)
