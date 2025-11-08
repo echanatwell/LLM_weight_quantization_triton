202fd5345e32b63dbd63040cd585303247721c51
Matthew <koltunov.matwei@gmail.com>
2025-11-09 00:36:54 +0500

    added linear triton and global quant triton

---
+++ CustomLayers/LinearTriton.py
@@ -0,0 +1,52 @@
+import torch
+import torch.nn as nn
+import triton
+import triton.language as tl
+
+@triton.jit
+def _matmul(x_ptr, w_ptr, out_ptr, B, IN, OUT, BLOCK_M: tl.constexpr, BLOCK_N: tl.constexpr, BLOCK_K: tl.constexpr):
+    pid_m = tl.prorgam_id(0)
+    pid_n = tl.program_id(1)
+
+    off_m = pid_m*BLOCK_M + tl.arange(0, BLOCK_M)
+    off_n = pid_n*BLOCK_N + tl.arange(0, BLOCK_N)
+
+    acc = tl.zeros((BLOCK_M, BLOCK_N), dtype=tl.float32)
+    for start in range(0, IN, BLOCK_K):
+        off_k = start + tl.arange(0, BLOCK_K)
+
+        off_x = off_m[:, None]*IN + off_k[None, :]
+        mask_x = (off_m[:, None] < B) & (off_k[None, :] < IN)
+        x = tl.load(x_ptr + off_x, mask=mask_x)
+
+        off_w = off_k[:, None]*OUT + off_n
+        mask_w = (off_k[:, None] < IN) & (off_n[None, :] < OUT)
+        w = tl.load(w_ptr + off_w, mask=mask_w)
+
+        acc += tl.dot(x, w, out_dtype=tl.float32)
+    
+    off_out = off_m[:, None]*OUT + off_n[None, :]
+    mask_out = (off_m[:, None] < B) & (off_n[None, :] < OUT)
+    tl.store(out_ptr + off_out, acc, mask=mask_out)
+
+
+class LinearTriton(nn.Module):
+    def __init__(self, l):
+        super(LinearTriton, self).__init__()
+        self.weight = nn.Parameter(l.weight.clone())
+    
+    def forward(self, x):
+        B, IN = x.size()
+        OUT, _ = self.weight
+        
+        BLOCK_M = 64
+        BLOCK_N = 64
+        BLOCK_K = 64
+        grid = (triton.cdiv(B, BLOCK_M), triton.cdiv(OUT, BLOCK_N))
+        out = torch.empty((B, OUT), dtype=torch.float32, device=x.device)
+        _matmul[grid](x, self.weight.T, out,
+                             B, IN, OUT,
+                             BLOCK_M, BLOCK_N, BLOCK_K)
+        out = out.to(x.dtype)
+        
+        return out
--- README.md
+++ README.md
@@ -9,5 +9,6 @@
 5. Квантование линейных слоев через pytorch (?)
 6. Замер скорости операции MM W16 vs W4 (SeqLen: 128, 512, 2048, LinLayer: 2048x2048, 2048x512, 2048x8192, 2048x128256)
 ## Triton kernels
-7. Кернел triton *(7.1)* квантование + *(7.2)* упаковка
-8. Кернел triton умножение матрицы bf16 на int4
+7. Global Quantization (per-tensor) + *(7.1)* упаковка
+8. Rowwise Quantization + *(8.1)* упаковка
+9. Matmul bf16 x int4
