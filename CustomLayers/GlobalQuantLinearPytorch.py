457d31dd10041898b233cfe3d2f5759862355ccb
Matthew <koltunov.matwei@gmail.com>
2025-11-13 00:10:53 +0500

    restore prev commits

--- CustomLayers/GlobalQuantLinearPytorch.py
+++ CustomLayers/GlobalQuantLinearPytorch.py
@@ -1,3 +1,7 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
 class QuantizedLinearGlobalTorch(nn.Module):
     def __init__(self, original_layer, device):
         super().__init__()
