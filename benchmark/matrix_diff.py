6e37de7a3551a6bf00709917fa1f4c490a0b621e
deniskirbaba <kirbabadenis@gmail.com>
2025-11-16 15:23:16 +0300

    rm duplicate

--- benchmark/matrix_diff.py
+++
@@ -1,8 +0,0 @@
-import torch
-
-
-def matrices_mse(m1: torch.tensor, m2: torch.tensor) -> float:
-    assert m1.shape == m2.shape
-    assert m1.dtype == m2.dtype
-
-    return float(((m1 - m2) ** 2).mean())
